48bc6cf7b1e1ce2f00823b83c077f2f44935fa80
JocoGar <1josejoaquin1@gmail.com>
2026-04-01 16:12:49 -0600

    Joaquín García -> prueba de rama

---
+++ src/proyectocompilador/prueba.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocompilador;
+
+/**
+ *
+ * @author 1jose
+ */
+public class prueba {
+    
+    /*
+    ESTO ES UNA PRUEBA
+    ESTO ES UNA PRUEBA
+    ESTO ES UNA PRUEBA
+    ESTO ES UNA PRUEBA
+    ESTO ES UNA PRUEBA
+    ESTO ES UNA PRUEBA
+    */
+}
